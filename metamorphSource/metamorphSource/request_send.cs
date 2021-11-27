0611d96d906414ef3f7ad38d341c20be0de50c8d
Your Name <you@example.com>
2021-11-27 17:28:49 +0100

    Added request send

---
+++ metamorphSource/metamorphSource/request_send.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace metamorphSource
+{
+    public class request_send
+    {
+        
+        //methode
+
+        
+        public async Task request_sender()
+        {
+            string headeredit = ""; //type de header a edit
+            string valuedit = ""; //valeur du type a edit 
+            List<string> methode = new List<string>(){"GET", "POST", "PUT"}; //GET, POST, PUT;
+            int choix_methode = 0; //par defaut GET
+            
+            string url = "";
+            
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod(methode[choix_methode]), url))
+                {
+                    request.Headers.TryAddWithoutValidation(headeredit, valuedit); 
+
+                    var response = await httpClient.SendAsync(request);
+                    Console.WriteLine("Request : \t" + request.Content);
+                    Console.WriteLine("Response : \t" + response.Content);
+                }
+            }
+        }
+    }
+}
